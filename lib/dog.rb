a16d6748d51e2155b20d4854d7bc164f5180d014
Alma Flores <flores.almalee@gmail.com>
2020-08-25 01:54:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   
   def initialize(name)
     @name = name
-    self.save << self
+    save << self
   end 
   
   
@@ -21,7 +21,7 @@ class Dog
     end
   end
   
-  def self.save 
+  def save 
     @@all << self 
   end
   
