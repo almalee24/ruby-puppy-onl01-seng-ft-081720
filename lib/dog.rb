92b26445cfbcbf38546acd18d45ade3171864a2d
Alma Flores <flores.almalee@gmail.com>
2020-08-25 01:49:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   @@all = []
   
   def initialize
-    
+    save
   end 
   
   private
@@ -22,5 +22,9 @@ class Dog
   
   def save 
     @@all << self 
+  end
+  
+  def clear_all 
+    @@all = []
   end
 end 
